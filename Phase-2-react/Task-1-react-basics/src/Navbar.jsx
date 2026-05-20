86678b03c1088820840e3b794c3cfd3ada5cac99
xiAvalaon <av.ammar7@gmail.com>
2026-05-20 16:16:37 +0300

    Update Task 1: rename blog title and add blog-list styling

--- Phase-2-react/Task-1-react-basics/src/Navbar.jsx
+++ Phase-2-react/Task-1-react-basics/src/Navbar.jsx
@@ -1,7 +1,7 @@
 function Navbar() {
     return (
         <nav className="navbar">
-            <h1>The Dojo Blog</h1>
+            <h1>My Blog</h1>
             <div className="links">
                 <a href="/">Home</a>
                 <a href="/create">
